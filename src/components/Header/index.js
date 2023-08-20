3bfd0bc670aabeb5eb02a3a7a72fedf15ba0b28a
Austin Grech <grech.austin@yahoo.ca>
2023-08-20 12:15:33 -0400

    changes to overall visuals

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,11 +1,11 @@
 import React from "react";
-import coverImage from "../../assets/cover/cover-image.jpg";
+import coverImage from "../../assets/cover/cover-image-2.jpg";
 
 function Header(props) {
   return (
     <header className="flex-row space-between px-1">
       <h1>Austin Grech</h1>
-      <img src={coverImage} alt="wooden background"></img>
+      <img src={coverImage} alt="coding background"></img>
       {props.children}
     </header>
   );
